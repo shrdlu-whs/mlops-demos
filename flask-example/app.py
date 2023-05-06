923600cca88ae0eb9556053d3b636c674994df95
Caren Daniel <caren.daniel@studmail.w-hs.de>
2023-05-06 19:21:27 +0200

    build: load jit trace of models

--- flask-example/app.py
+++ flask-example/app.py
@@ -1,24 +1,72 @@
+import os
 # %%
-from transformers import AutoTokenizer
-from transformers import pipeline
+import torch
+from tokenizers import Tokenizer
+from transformers import DistilBertTokenizer
 
 # %%
 MODEL_VERSION = "1"
 MODEL_PATH_EMOTION = "./saved-models/distilbert-emotion/" + MODEL_VERSION
 MODEL_PATH_SENTIMENT = "./saved-models/distilbert-sentiment/" + MODEL_VERSION
 TOKENIZER = "distilbert-base-uncased"
+SEQUENCE_LENGTH = 64
 #%%
-tokenizer = AutoTokenizer.from_pretrained(TOKENIZER)
+tokenizer = Tokenizer.from_pretrained(TOKENIZER)
+sentiment_labels = ['negative','positive']
+emotion_labels = ['sadness','joy', 'love', 'anger', 'fear', 'surprise']
+#%%
+def load_model(model_dir):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    loaded_model = torch.jit.load(os.path.join(model_dir, "model.pt"))
+    print("model loaded from "+ model_dir)
+    
+    return loaded_model.to(device)
+
+def encode_input(data):
+        if isinstance(data, str):
+            data = [data]
+        elif isinstance(data, list) and len(data) > 0 and isinstance(data[0], str):
+            pass
+        else:
+            raise ValueError("Unsupported input type. Input type can be a string or an non-empty list. \
+                             I got {}".format(data))
+        encoded_sentences = [tokenizer.encode(x, add_special_tokens=True) for x in data]
+        # pad shorter sentence
+        padded =  torch.zeros(len(encoded_sentences), SEQUENCE_LENGTH) 
+        for i, s in enumerate(encoded_sentences):
+            padded[i, :len(s.ids)] = torch.tensor(s.ids)
+     
+        # create mask
+        mask = (padded != 0)
+
+        return padded.long(), mask.long()
+
+import torch.nn.functional as F
+def predict(input_data, model):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    model.eval()
+
+    input_id, input_mask = input_data
+    input_id = input_id.to(device)
+    input_mask = input_mask.to(device)
+    
+    # Do inference
+    with torch.no_grad():
+        y = model(input_id, attention_mask=input_mask)[0]
+    probabilities = F.softmax(y, dim=-1)
+    return probabilities
+
+#%%
 # Load model for text classification by emotion:
 # sadness, joy, fear, anger, surprise
-emotion_model = pipeline("text-classification", model=MODEL_PATH_EMOTION, top_k=None)
+emotion_model = load_model(MODEL_PATH_EMOTION)
 # Load model for text classification by broad sentiment only:
 # positive/negative
-sentiment_model = pipeline("text-classification", model=MODEL_PATH_SENTIMENT)
-
-# %%
+sentiment_model = load_model(MODEL_PATH_SENTIMENT)
+#%%
 from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
+from flask_restful import reqparse, Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
@@ -30,12 +78,15 @@ parser.add_argument('query', type=str, location='args')
 # Returns max sentiment prediction and confidence for a given query text
 class SentimentPrediction(Resource):
     def get(self):
-        # use parser and find the user's query
+        # use parser and find the users query
         args = parser.parse_args()
         user_query = args['query']
-        sentiment_prediction = sentiment_model(user_query)
-        sentiment = sentiment_prediction[0]['label'].lower()
-        confidence = round(sentiment_prediction[0]['score'],4)
+        input_data = encode_input(user_query)
+        sentiment_prediction = predict(input_data, sentiment_model)[0]
+        sentiment_idx = torch.argmax(sentiment_prediction)
+        sentiment_prediction = sentiment_prediction.detach().cpu().numpy()
+        sentiment = sentiment_labels[sentiment_idx]
+        confidence = float(round(sentiment_prediction[sentiment_idx],4))
 
         # create JSON object
         output = {"sentiment": sentiment, "confidence": confidence}
@@ -47,19 +98,21 @@ class EmotionPrediction(Resource):
         # Use parser and find the user's query
         args = parser.parse_args()
         user_query = args['query']
+        input_data = encode_input(user_query)
         # predict emotion
-        emotion_prediction = emotion_model(user_query)
-        max_prediction = max(emotion_prediction[0], key=lambda x:x['score'])
-        emotion = max_prediction['label']
-        confidence = round(max_prediction['score'],4)
-        
-        # Create JSON object
+        emotion_prediction = predict(input_data, emotion_model)[0]
+        emotion_idx = torch.argmax(emotion_prediction)
+        emotion_prediction = emotion_prediction.detach().cpu().numpy()
+        emotion = emotion_labels[emotion_idx]
+        confidence = float(round(emotion_prediction[emotion_idx],4))
+
+        # create JSON object
         output = {"emotion": emotion, "confidence": confidence}
         return output
 
 #%%
 # %%
-# Returns all availanle models with description
+# Returns all available models with description
 class Models(Resource):
     def get(self):
 
--- flask-example/save-models.py
+++ flask-example/save-models.py
@@ -1,15 +1,39 @@
 # %%
 # Save models for use in Flask app and Docker container
-from transformers import pipeline
+import os
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+import torch
 #%%
 MODEL_VERSION = "1"
-MODEL_PATH_EMOTION = "./saved-models/distilbert-emotion/" + MODEL_VERSION
-MODEL_PATH_SENTIMENT = "./saved-models/distilbert-sentiment/" + MODEL_VERSION
+MODEL_PATH_EMOTION = os.path.join("./saved-models/distilbert-emotion/", MODEL_VERSION)
+MODEL_PATH_SENTIMENT = os.path.join("./saved-models/distilbert-sentiment/", MODEL_VERSION)
+SEQUENCE_LENGTH = 64
+JIT_EXPORT = True
+
+device = device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+
 # Load model for text classification by emotion:
 # sadness, joy, fear, anger, surprise
-emotion_model = pipeline("text-classification", model="bhadresh-savani/distilbert-base-uncased-emotion", top_k=None)
-emotion_model.save_pretrained(MODEL_PATH_EMOTION)
+emotion_model = DistilBertForSequenceClassification.from_pretrained("bhadresh-savani/distilbert-base-uncased-emotion", top_k=None, torchscript=True)
 # Load model for text classification by broad sentiment only:
 # positive/negative
-sentiment_model = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
-sentiment_model.save_pretrained(MODEL_PATH_SENTIMENT)
+sentiment_model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english", torchscript=True)
+# %%
+# Save Jit model trace
+if JIT_EXPORT:
+    # Create dummy input for trace
+    for_jit_trace_input_ids = [0] * SEQUENCE_LENGTH
+    for_jit_trace_attention_masks = [0] * SEQUENCE_LENGTH
+    for_jit_trace_input = torch.tensor([for_jit_trace_input_ids])
+    for_jit_trace_masks = torch.tensor([for_jit_trace_input_ids])
+
+    traced_emotion_model = torch.jit.trace(
+        emotion_model, [for_jit_trace_input.to(device), for_jit_trace_masks.to(device)]
+    )
+    torch.jit.save(traced_emotion_model, os.path.join(MODEL_PATH_EMOTION, "model.pt"))
+
+    traced_sentiment_model = torch.jit.trace(
+        sentiment_model, [for_jit_trace_input.to(device), for_jit_trace_masks.to(device)]
+    )
+    torch.jit.save(traced_sentiment_model, os.path.join(MODEL_PATH_SENTIMENT, "model.pt"))
